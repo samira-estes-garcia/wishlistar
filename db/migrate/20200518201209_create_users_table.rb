4768cac10c7084903adce0223da5c71c9350d3e4
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-05-18 16:00:39 -0500

    created user table and wishlist table migrations

--- db/migrate/20200518201209_create_users_table.rb
+++ db/migrate/20200518201209_create_users_table.rb
@@ -1,9 +1,11 @@
 class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :name
       t.string :email
       t.string :password_digest
+
+      t.timestamps null: false
     end
   end
 end
