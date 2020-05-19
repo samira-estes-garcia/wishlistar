fb244c061213e99299a1c4e19f064ddf31d1b623
samira-estes-garcia <samira.estes.garcia@gmail.com>
2020-05-18 19:03:49 -0500

    add login and signup, enabled sessions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,30 @@ class UsersController < ApplicationController
         erb :login
     end
 
+    #receive login form, find the user, and log the user in (create session)
+    post '/login' do 
+        #find the user
+        @user = User.find_by(email: params[:email])
+        #authenticate the user
+        if @user.authenticate(params[:password])
+            #log the user in - create the user session
+            session[:user_id] = @user.id
+            #redirect the user
+            redirect :"users/#{@user.id}"
+        else
+            #tell user they entered invalid credentials
+            #redirect them to the login page
+        end
+    end
+
     get '/signup' do 
+    end
+
+    post '/signup' do 
+    end
+
+    get '/users/:id' do 
+        #this will be the users show page
     end
 
 end
